d0bff23aea867ada331d1cf8ca65ea6cb927f1e2
Carlos Barbosa <carlosha.barbosa@gmail.com>
2017-09-22 14:20:00 -0300

    finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,21 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            reder 'edit'
+        end
+        
+    end
     
     def create
         @article = Article.new(article_params)
@@ -19,6 +34,14 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+
+        @article.destroy
+
+        redirect_to articles_path
     end
 
     private
